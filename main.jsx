bdf386f270dcc57fa34b8ae653ad399a442a649a
contractoriq-wyk <wyktrckllc@gmail.com>
2026-04-18 04:07:44 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ContractorIQv26 from './contractoriq.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(ContractorIQv26)
+)
